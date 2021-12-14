428f8dbd3a50f1075d56514caac37307b566cd14
trunglan2101 <trunglan2101@gmail.com>
2021-12-14 10:20:11 +0700

    chuong5rails

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # get 'user/new'
+  root 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/signup' => 'user#new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
